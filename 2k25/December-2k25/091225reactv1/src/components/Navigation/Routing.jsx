974e7beafbe6678ed986a103b71395f60bc869e4
dev nayak10 <devprasannanayak@gmail.com>
2025-12-09 09:08:40 +0530

    content added

---
+++ 2k25/December-2k25/091225reactv1/src/components/Navigation/Routing.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+const Routing = () => {
+  return (
+    <div>
+        <BrowserRouter>
+            <Routes
+                
+            ></Routes>
+        </BrowserRouter>
+    </div>
+  )
+}
+
+export default Routing
