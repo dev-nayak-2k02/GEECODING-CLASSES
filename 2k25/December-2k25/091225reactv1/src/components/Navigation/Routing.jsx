d590292586f2ab728f15555f604e211e4a43abf6
dev nayak10 <devprasannanayak@gmail.com>
2025-12-09 09:50:50 +0530

    content added

--- 2k25/December-2k25/091225reactv1/src/components/Navigation/Routing.jsx
+++ 2k25/December-2k25/091225reactv1/src/components/Navigation/Routing.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import {BrowserRouter, Routes, Route,} from 'react-router-dom'
-import Main from "../../pages/main/Main";
+import React from 'react';
+import {BrowserRouter, Routes, Route,} from 'react-router-dom';
+import Main from "../../pages/main/Main.jsx";
 const Routing = () => {
   return (
     <div>
         <BrowserRouter>
             <Routes>
-              <Route path='/' element={<Main />}/>  
+              <Route path='/pages/main/Main' element={<Main />}/>  
             </Routes>
         </BrowserRouter>
     </div>
