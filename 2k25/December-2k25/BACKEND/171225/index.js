caf50148d257b98d035b24db4de0947001dc1e26
dev nayak10 <devprasannanayak@gmail.com>
2025-12-17 08:41:28 +0530

    content added

---
+++ 2k25/December-2k25/BACKEND/171225/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/search/:id', (req,res) =>{
+    console.log(req.params.id)
+})
+app.listen(PORT, ()=>{
+    console.log(`sever in running at port: ${PORT}`)
+})
