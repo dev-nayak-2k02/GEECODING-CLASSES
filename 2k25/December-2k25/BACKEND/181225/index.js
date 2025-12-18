e3334e0dc32330cdee7f988927fb51302768e0cc
dev nayak10 <devprasannanayak@gmail.com>
2025-12-18 08:53:06 +0530

    content added

--- 2k25/December-2k25/BACKEND/181225/index.js
+++ 2k25/December-2k25/BACKEND/181225/index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express();
 const PORT = 8000;
+let query = {
+    userName: 'Dev',
+    passWord: '123554@'
+}
+app.get('/search', (req, res) => {
+    console.log(req.query);
+})
 app.listen(PORT, () => {
     console.log(`server is running at PORT: ${PORT}`)
 })
