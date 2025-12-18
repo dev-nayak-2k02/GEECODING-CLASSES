e4dede5dc527d497cebb0183d9892db9096195b7
dev nayak10 <devprasannanayak@gmail.com>
2025-12-18 09:30:39 +0530

    content added

--- 2k25/December-2k25/BACKEND/181225/index.js
+++ 2k25/December-2k25/BACKEND/181225/index.js
@@ -1,13 +1,36 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 const PORT = 8000;
-let query = {
-    userName: 'Dev',
-    passWord: '123554@'
-}
-app.get('/search', (req, res) => {
-    console.log(req.query);
-})
+
+let students = [
+    { id: 13, name: 'devNayak' },
+    { id: 12, name: 'subham' },
+    { id: 11, name: 'ram' }
+];
+
+app.get('/students', (req, res) => {
+    const { id, name } = req.query;
+
+    if (!id || !name) {
+        console.error('❌ id or name missing');
+        return res.send('Check terminal');
+    }
+
+    const student = students.find(
+        s => s.id === Number(id) && s.name === name
+    );
+
+    if (!student) {
+        console.error(`❌ No student found with id=${id} and name=${name}`);
+        return res.send('Check terminal');
+    }
+
+    console.log('✅ Student Found:', student);
+    res.send('Check terminal');
+});
 app.listen(PORT, () => {
-    console.log(`server is running at PORT: ${PORT}`)
-})
+    console.log(`server is running at port: ${PORT}`);
+});
+
+
+//GET /students?id=13&name=dev
